0f313ce123fd1eaa5ddb0db67a10653c5a8b24f6
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-02 17:38:43 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState('내가 쓴 글 목록');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header className="nav">
+        <div className="nav-1">
+          <i class="fas fa-frog"></i>
+          <a href="">자유 게시판</a>
+        </div>
+        <div className="nav-2">
+          <a className="login" href="">log in</a>
+          <a className="signup" href="">sign up</a>
+        </div>
       </header>
+
+      <div className="contents">
+        <div className="contents-1">
+          글 목록
+        </div>
+        <div className="contents-2">
+          { 글제목 }
+        </div>
+      </div>
+
     </div>
   );
+
 }
 
 export default App;
