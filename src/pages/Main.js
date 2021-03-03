18589faf95b8986e68d534fde498d9ef55dcd96f
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-03 17:40:38 +0900

    update

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,20 +1,56 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
-function Main() {
-    let [글제목, 글제목변경] = useState('내가 쓴 글 목록');
-   
+function Postsprint({id, date, title, string}){
+    return( 
+        <div className="contents-2">
+            <p>제목 : {title}</p>
+            <p>글쓴이 : {id} 날짜 : date</p>
+            <p>{string}</p>
+        </div>
+        );
+}
+
+
+function Main() {   
+    let users =[
+        {
+            id: "chltpgus@naver.com",
+            date: "2021-10-21",
+            title: "점심",
+            string: "오늘은 맛있는 밥을 먹었다 맛있었다 암냠냠"
+        },
+        {
+            id: "tjdbsdk@naver.com",
+            date: "2021-09-03",
+            title: "아 배아프다",
+            string: "님덜 똥마려운데 회사째고 집으로 똥싸러 갈까요?"
+        },
+        {
+            id: "skatlsdnr@naver.com",
+            date: "2021-05-18",
+            title: "인천 월미도",
+            string: "월미도 바이킹 재밌네 여자친구랑도 가야지 물론 여자친구 만저 사겨야지함"
+        }
+]
+    let [글제목, 글제목변경] = useState(users);
+    console.log(글제목);
     return (
         <div className="contents">
-                <div className="contents-1">
-                    글 목록
+            <div className="contents-1">
+                <div className="postlist">
+                    <Link >전체 글 목록</Link>
+                    <Link >내가 쓴 글 목록</Link>
                 </div>
-                <div className="contents-2">
-                    {글제목}
+                <div className="newpost">
+                    <Link >새로운 글 작성</Link>
                 </div>
             </div>
+            {users.map(posts => <Postsprint id={posts.id} date={posts.date} title={posts.title} string={posts.string} />)}
+        </div>
     );
-  
+
   }
 
 export default Main;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,11 +1,36 @@
 import React, { useState } from 'react';
-
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function Signup() {
    
+    
+
     return (
-        <div>
-            회원가입
+        <div className = "Signuppage">
+            <div>
+                <p className="signuptext">회원가입</p>
+                <p className="signuptext2">회원 가입 후 컨텐츠 이용이 가능합니다.</p>
+            </div>
+            <div className="ID">
+                <input type="text" className="INPUT02" placeholder="이메일 주소" />
+                <p>{}</p>
+            </div>
+            <div className="PASS">
+                <input type="password" className="INPUT02" placeholder="암호" />
+            </div>
+            <div className="PASS2">
+                <input type="password" className="INPUT02" placeholder="암호 확인" />
+            </div>
+            <div className="NICKNAME">
+                <input type="text" className="INPUT02" placeholder="계정 이름" />
+            </div>
+            <div className="logbtndiv">
+                <Link className="loginbtn">게시판 회원가입</Link>
+            </div>
+            <div className="logsign">
+                <Link className="loginbtn2" to="/login">로그인</Link>
+            </div>
         </div>
     );
   
