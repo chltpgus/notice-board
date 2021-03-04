d30da2c121491b4b549b79dbeb1bfaaf355c40a6
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-04 23:39:26 +0900

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -35,8 +35,7 @@ function Signup() {
             nickname: nicknameInput
         };
 
-        fetch('https://noticeboardserverr.herokuapp.com/signup', {method:'post'}) //fetch로 서버에게 요청하고 테스트한 사용자 인원수를 가져와 출력한다.
-            .then(function (res) {
+       
                 var xhr = new XMLHttpRequest();
                 var url = "https://noticeboardserverr.herokuapp.com/signup";
                 xhr.open("POST", url, true);
@@ -64,7 +63,7 @@ function Signup() {
                 console.log(users)
                 var data = JSON.stringify(users);
                 xhr.send(data);
-            });
+           
  
 
 
