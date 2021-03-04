98026b08fd332c0a25aef76a6951a43c02b39078
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-04 21:00:03 +0900

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -41,7 +41,7 @@ function Signup() {
                 console.log(res[0]);    
                 users = res;
             });
-            console.log(Object.keys(users).length);
+            //console.log(Object.keys(users).length);
 
         if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailInput)){
             setEmailError(false);
