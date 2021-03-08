574d63a703684733f9601edcfaf97b1418a67012
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-08 18:52:47 +0900

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -48,14 +48,6 @@ function Signup() {
   
         
 
-        console.log("emailError : " + emailError);
-        console.log("emailoverlapError : " + emailoverlapError);
-        console.log("passwordError : " + passwordError);
-        console.log("nicknameError : " + nicknameError);
-        console.log("emailError02 : " + emailError02);
-        console.log("emailoverlapError02 : " + emailoverlapError02);
-        console.log("passwordError02 : " + passwordError02);
-        console.log("nicknameError02 : " + nicknameError02);
 
 
 
@@ -111,6 +103,14 @@ function Signup() {
                                     setPasswordError(false);
                                     passwordError02 = false;
                                 }
+                                console.log("emailError : " + emailError);
+                                console.log("emailoverlapError : " + emailoverlapError);
+                                console.log("passwordError : " + passwordError);
+                                console.log("nicknameError : " + nicknameError);
+                                console.log("emailError02 : " + emailError02);
+                                console.log("emailoverlapError02 : " + emailoverlapError02);
+                                console.log("passwordError02 : " + passwordError02);
+                                console.log("nicknameError02 : " + nicknameError02);
             
                             }); 
                     
