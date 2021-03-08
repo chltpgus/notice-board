f4c3710ebed68290e6cae4d422a4d9ac29711c99
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-08 19:16:45 +0900

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -15,7 +15,7 @@ function Signup() {
     const [passwordError, setPasswordError] = useState(false);
     const [nicknameError, setNicknameError] = useState(false);
 
-    const [signupto, setSignupto] = useState("");
+    const [signupto, setSignupto] = useState("/singup");
 
 
     
