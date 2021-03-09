28c5bcbeac5d54e4e2388de98b64aca56f248874
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-09 18:21:13 +0900

    sd

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import { Link, RouteComponentProps } from 'react-router-dom';
 
 function Header() {
     const [loginout, setLoginout] = useState(false);
+    let user = JSON.parse(sessionStorage.getItem('USER'));
 
     if(user === null){
         setLoginout(false);
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -39,8 +39,8 @@ function Main() {
     let [글제목, 글제목변경] = useState(users);
     //console.log(글제목);
 
-    let user = JSON.parse(sessionStorage.getItem('USER'));
-    console.log(user);
+    //let user = JSON.parse(sessionStorage.getItem('USER'));
+    //console.log(user);
 
     return (
         <div className="contents">
