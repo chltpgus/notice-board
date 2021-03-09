8fb848fdf7e67f481819b457226cac3860ac0723
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-09 18:58:59 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,14 @@ import { Link, RouteComponentProps } from 'react-router-dom';
 
 
 function Header() {
-    const [loginout, setLoginout] = useState(false);
+    let user = JSON.parse(sessionStorage.getItem('USER'));
+    if(user === null){
+        const [loginout, setLoginout] = useState(false);
+    }
+    else {
+        const [loginout, setLoginout] = useState(true);
+    }
+   
     
 
     const handleClick = () => {
