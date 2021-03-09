3e7418a6ec279820bc75390de60ad41f2bef88e7
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-09 18:36:59 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,17 @@ import { Link, RouteComponentProps } from 'react-router-dom';
 function Header() {
     const [loginout, setLoginout] = useState(false);
     let user = JSON.parse(sessionStorage.getItem('USER'));
+    if(user === null){
+        setLoginout(false);
+        console.log(user);
+    }
+    else if(user !== null){
+        setLoginout(true);
+        console.log(user);
+    }
 
     const handleClick = () => {
-        if(user === null){
-            setLoginout(false);
-        }
-        else{
-            setLoginout(true);
-        }
-        console.log(user);
         sessionStorage.setItem('USER', null);  
-    }
-    const handleClick2 = () => {
-        if(user === null){
-            setLoginout(false);
-        }
-        else{
-            setLoginout(true);
-        }
-        console.log(user);
     }
 
     return (
@@ -33,8 +25,8 @@ function Header() {
             <Link className="logo" to="/" onClick={handleClick2}>자유 게시판</Link>
         </div>
         <div className="nav-2">
-            {!loginout && <Link  className="login" to="/login"onClick={handleClick2} >Log in</Link>}
-          {!loginout && <Link className="signup" to="/signup" onClick={handleClick2}>sign up</Link>}
+            {!loginout && <Link  className="login" to="/login" >Log in</Link>}
+          {!loginout && <Link className="signup" to="/signup" >sign up</Link>}
             { loginout && <Link  className="logout" onClick={handleClick} >Log out</Link>}
         </div>
     </header>
