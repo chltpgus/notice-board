a33dcc06498a04d860c7bdf1f0d260660dc033c5
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-09 16:52:38 +0900

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -38,7 +38,7 @@ function Main() {
 ]
     let [글제목, 글제목변경] = useState(users);
     //console.log(글제목);
-    let user = JSON.parse(localStorage.getItem('USER'));
+    let user = (localStorage.getItem('USER'));
 
     return (
         <div className="contents">
