3a42de8f5b02eb51e23b1a1e90b0c041ea06b751
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-09 17:27:58 +0900

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -39,8 +39,8 @@ function Main() {
     let user;
     let [글제목, 글제목변경] = useState(users);
     //console.log(글제목);
-    
-    if (sessionStorage.getItem('USER') === "NULL") {
+
+    if (JSON.parse(sessionStorage.getItem('USER')) === "NULL") {
         user = JSON.parse(sessionStorage.getItem('USER'));
     }
     console.log(user);
