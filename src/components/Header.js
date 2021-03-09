b84930840ae5d35104d91acdb7efb592e5d82512
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-09 18:41:43 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,24 +5,33 @@ import { Link, RouteComponentProps } from 'react-router-dom';
 function Header() {
     const [loginout, setLoginout] = useState(false);
     let user = JSON.parse(sessionStorage.getItem('USER'));
-    if(user === null){
-        setLoginout(false);
-        console.log(user);
-    }
-    else if(user !== null){
-        setLoginout(true);
-        console.log(user);
-    }
 
     const handleClick = () => {
         sessionStorage.setItem('USER', null);  
+
+        if(user === null){
+            setLoginout(false);
+        }
+        else{
+            setLoginout(true);
+        }
+        console.log(user);
+    }
+    const handleClick2 = () => {
+        if(user === null){
+            setLoginout(false);
+        }
+        else{
+            setLoginout(true);
+        }
+        console.log(user);
     }
 
     return (
         <header className="nav">
         <div className="nav-1">
             <i className="fas fa-frog"></i>
-            <Link className="logo" to="/" >자유 게시판</Link>
+            <Link className="logo" to="/" onClick={handleClick2}>자유 게시판</Link>
         </div>
         <div className="nav-2">
             {!loginout && <Link  className="login" to="/login" >Log in</Link>}
