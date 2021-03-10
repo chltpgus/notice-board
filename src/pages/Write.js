d5afb35ec8c76a0cda4a86e673240d520f23c703
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-10 16:34:40 +0900

    Update Write.js

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -13,6 +13,8 @@ function Write() {
             <div>
             <input type="text" id="name" className="INPUT03" placeholder="제목" />
             <input type="text" id="name" className="INPUT03" placeholder="본문 내용" />
+            <Link className="logout">저장</Link>
+            <Link className="logout">취소</Link>
             </div>
             <div>
             <Link className="logout">저장</Link>
