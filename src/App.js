bd715715bb72e15f8bb7374813d297ede706746a
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-10 15:58:28 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import Main from './pages/Main';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Write from './pages/Write';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" exact component={Main}/>
         <Route path="/login" exact component={Login}/>
         <Route path="/signup" exact component={Signup}/>
+        <Route path="/write" exact component={Write}/>
         <Footer/>
       </div>
     </BrowserRouter>
