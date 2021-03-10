07c06c53908610bb6b1e5bb0969b20d4af26fba4
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-10 16:29:25 +0900

    Update Write.js

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -10,7 +10,14 @@ function Write() {
  
     return (
         <div className="Writepage">
-          sdf
+            <div>
+            <input type="text" id="name" className="INPUT02" placeholder="제목" />
+            <input type="text" id="name" className="INPUT02" placeholder="본문 내용" />
+            </div>
+            <div>
+            <Link className="logout"  >저장</Link>
+            <Link className="logout"  >취소</Link>
+            </div>
         </div>
     );
 
