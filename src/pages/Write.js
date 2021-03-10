bd1e325ae4f86212ba044b470b6ac93daf07082d
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-10 22:59:29 +0900

    Update Write.js

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, useHistory  } from 'react-router-dom';
 
 
 
