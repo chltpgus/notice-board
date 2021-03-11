3568e991e34bd000df48c843808436465eba3e47
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-11 22:48:34 +0900

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -56,7 +56,7 @@ function Main() {
         <div className="contents" >
             <div className="contents-1">
                 <div className="postlist">
-                    <Link to="/mytext" className="mainbtn" >내가 쓴 글 목록</Link>
+                    <Link  className="mainbtn" >내가 쓴 글 목록</Link>
                 </div>
                 <div className="newpost">
                     <Link to="/write" className="mainbtn">새로운 글 작성</Link>
