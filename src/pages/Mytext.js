50fabcd646341951059dfa5a7cbcdbb061fabfde
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-11 23:51:51 +0900

    Update Mytext.js

--- src/pages/Mytext.js
+++ src/pages/Mytext.js
@@ -7,8 +7,15 @@ function Mytext() {
     
 
     return (
-        <div className="loginpage">
-            sdfs
+        <div className="contents" >
+            <div className="contents-1">
+                <div className="postlist">
+                    <Link to="/" className="mainbtn" >모든 글 목록</Link>
+                </div>
+                <div className="newpost">
+                    <Link to="/write" className="mainbtn">새로운 글 작성</Link>
+                </div>
+            </div>
         </div>
     );
   
