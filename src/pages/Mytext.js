9bf68365f83d6633d6372dc18484d903ed2cce16
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-11 23:49:07 +0900

    sd

---
+++ src/pages/Mytext.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { BrowserRouter, Route, useHistory  } from 'react-router-dom';
+import Signup from './Signup';
+
+function Login() {
+    
+
+    return (
+        <div className="loginpage">
+            sdfs
+        </div>
+    );
+  
+  }
+
+export default Login;
