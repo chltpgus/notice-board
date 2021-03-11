de859c4f6b96d4ee86db58eab19791e7247a5f5c
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-11 23:03:16 +0900

    Update Mytext.js

--- src/pages/Mytext.js
+++ src/pages/Mytext.js
@@ -2,9 +2,36 @@ import React, { useState } from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { BrowserRouter, Route, useHistory  } from 'react-router-dom';
 import Signup from './Signup';
-
+function Postsprint({nickname, title, date, maintext}){
+    return( 
+        <div className="contents-2">
+            <h3>{title}</h3>
+            <p>{nickname}  {date}</p>
+            <p>{maintext}</p>
+        </div>
+        );
+}
 function Mytext() {
-    
+    let users = [/*
+        {
+            id: "chltpgus@naver.com",
+            date: "2021-10-21",
+            title: "점심",
+            string: "오늘은 맛있는 밥을 먹었다 맛있었다 암냠냠"
+        },
+        {
+            id: "tjdbsdk@naver.com",
+            date: "2021-09-03",
+            title: "아 배아프다",
+            string: "님덜 똥마려운데 회사째고 집으로 똥싸러 갈까요?"
+        },
+        {
+            id: "skatlsdnr@naver.com",
+            date: "2021-05-18",
+            title: "인천 월미도",
+            string: "월미도 바이킹 재밌네 여자친구랑도 가야지 물론 여자친구 만저 사겨야지함"
+        }*/
+    ]
     let [users2, users2change] = useState(users);
     let user = JSON.parse(sessionStorage.getItem('USER'));
 
