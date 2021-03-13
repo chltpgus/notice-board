68ff44acc3a69a1ea75d389eb062aa0bce800229
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-13 16:52:22 +0900

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 
 function Postsprint({nickname, title, date, maintext}){
@@ -15,27 +15,8 @@ function Postsprint({nickname, title, date, maintext}){
 
 
 function Main() {   
-    let users = [/*
-        {
-            id: "chltpgus@naver.com",
-            date: "2021-10-21",
-            title: "점심",
-            string: "오늘은 맛있는 밥을 먹었다 맛있었다 암냠냠"
-        },
-        {
-            id: "tjdbsdk@naver.com",
-            date: "2021-09-03",
-            title: "아 배아프다",
-            string: "님덜 똥마려운데 회사째고 집으로 똥싸러 갈까요?"
-        },
-        {
-            id: "skatlsdnr@naver.com",
-            date: "2021-05-18",
-            title: "인천 월미도",
-            string: "월미도 바이킹 재밌네 여자친구랑도 가야지 물론 여자친구 만저 사겨야지함"
-        }*/
-    ]
-
+    let users = []
+    const history = useHistory();
     let [users2, users2change] = useState(users);
     let user = JSON.parse(sessionStorage.getItem('USER'));
  
@@ -53,14 +34,29 @@ function Main() {
 
     setTimeout(datafetch, 0);
 
+    const writehandleClick = () => {
+        if(user === null){
+            history.push('/login');
+        } else{
+            history.push('/write');
+        }
+    }
+    const mytexthandleClick = () => {
+        if(user === null){
+            history.push('/login');
+        } else{
+            history.push('/mytext');
+        }
+    }
+
     return (
         <div className="contents" >
             <div className="contents-1">
                 <div className="postlist">
-                    <Link to="/mytext" className="mainbtn" >내가 쓴 글 목록</Link>
+                    <Link onClick = {mytexthandleClick} className="mainbtn" >내가 쓴 글 목록</Link>
                 </div>
                 <div className="newpost">
-                    <Link to="/write" className="mainbtn">새로운 글 작성</Link>
+                    <Link onClick={writehandleClick} className="mainbtn">새로운 글 작성</Link>
                 </div>
             </div>
             {users2.map(posts => <Postsprint nickname={posts.nickname} title={posts.title} date={posts.date} maintext={posts.maintext} />)}
