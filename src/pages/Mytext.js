7408f95595cf52425dcd6393a52ea57615c3979c
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-13 13:53:12 +0900

    Update Mytext.js

--- src/pages/Mytext.js
+++ src/pages/Mytext.js
@@ -45,7 +45,7 @@ function Mytext() {
             .then(function (res) {
                 users = res;
                
-                for (let a = 0; a < res.lenth; a++) {
+                for (let a = 0; a < 3; a++) {
                     console.log("dk");
                 }
                 
