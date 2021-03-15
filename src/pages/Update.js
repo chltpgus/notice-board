0fcf66190fee51a982b3595d803a55fac96055bb
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-15 21:19:20 +0900

    Update Update.js

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -10,7 +10,7 @@ import { BrowserRouter, Route, useHistory, useLocation  } from 'react-router-dom
 function Update() {   
    //const getParms = this.props.location.state.detail;
    const location = useLocation();
-   const getParms = location.state.detail;
+   const getParms = location.state;
     return (
         <div className="Writepage">
            <p>{getParms}</p>
