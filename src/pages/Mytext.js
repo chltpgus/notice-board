0192f5e7592dd31a07b5358f9282701bd9d3df38
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-15 17:14:24 +0900

    Update Mytext.js

--- src/pages/Mytext.js
+++ src/pages/Mytext.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, useHistory  } from 'react-router-dom';
 import Signup from './Signup';
 
 function Postsprint({nickname, title, date, maintext}){
-    console.log(nickname);
+    console.log(title);
     return( 
         <div className="contents-2">
             <h3>{title}</h3>
