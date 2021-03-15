88903ed6e928fba42d81e413d97e54a22762515d
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-15 16:54:38 +0900

    df

--- src/pages/Mytext.js
+++ src/pages/Mytext.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { BrowserRouter, Route, useHistory  } from 'react-router-dom';
 import Signup from './Signup';
