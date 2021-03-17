0f463a7cc2b5ae10328ac74204e710e21dad6b6b
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-17 16:04:41 +0900

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -44,7 +44,7 @@ function Main() {
                 pages.push(a+1);
             }
             pagechange(pages);
-            console.log(page)
+            console.log(page);
         });
       }, []);
 
