7b6dd531ba9e52f2afd553e48d198077e02dca0a
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-17 18:59:07 +0900

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -81,16 +81,15 @@ function Main() {
         <div className="contents" >
             <div className="contents-1">
                 <div className="postlist">
-                    <Link onClick = {mytexthandleClick} className="mainbtn" >내가 쓴 글 목록</Link>
+                    <Link onClick={mytexthandleClick} className="mainbtn" >내가 쓴 글 목록</Link>
                 </div>
                 <div className="newpost">
                     <Link onClick={writehandleClick} className="mainbtn">새로운 글 작성</Link>
                 </div>
-            </div>
-            {users3.map(posts => <Postsprint nickname={posts.nickname} title={posts.title} date={posts.date} maintext={posts.maintext} />)}
-
-            <div className="contents-2">
-                <Pagination total={totalCount} current={currentPage} pageSize={pageSize} onChange={(page) => setCurrentPage(page)}/>
+                {users3.map(posts => <Postsprint nickname={posts.nickname} title={posts.title} date={posts.date} maintext={posts.maintext} />)}
+                <div className="contents-2">
+                    <Pagination total={totalCount} current={currentPage} pageSize={pageSize} onChange={(page) => setCurrentPage(page)} />
+                </div>
             </div>
         </div>
     );
