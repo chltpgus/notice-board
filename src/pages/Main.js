a3f528cf535e50450b077d55f7748ab4f4ea25a5
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-17 15:10:01 +0900

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -41,7 +41,7 @@ function Main() {
         });
       }, [users]);
 
-      console.log(Math.ceil((users2)/5));
+      console.log(((users2)/5));
 
     const writehandleClick = () => {
         if(user === null){
