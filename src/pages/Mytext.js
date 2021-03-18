37acc84c61a6858434b68d296fc25f84e8659617
chltpgus <67909892+chltpgus@users.noreply.github.com>
2021-03-18 11:46:57 +0900

    Update Mytext.js

--- src/pages/Mytext.js
+++ src/pages/Mytext.js
@@ -45,7 +45,7 @@ function Postsprint({nickname01, title01, date01, maintext01}){
                             maintext: maintext01
                         }
                     }} className="mainbtn">수정</Link>
-                    <Link onClick={handleClick} to="/" className="mainbtn">삭제</Link>
+                    <Link onClick={handleClick} to="/" className="delbtn">삭제</Link>
                 </div>
             </div>
             <div className="titledate">
